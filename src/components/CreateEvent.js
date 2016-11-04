53cb5418fa5c04a620bf502ec939163833696805
Stephen Michelini <sdmichelini@wpi.edu>
2016-11-04 14:43:31 -0400

    Bound form value to ReactState

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -5,25 +5,41 @@ import EventActions from '../actions/EventActions';
 import EventStore from '../stores/EventStore';
 
 class CreateEventComponent extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      pFor: 0,
+      pAgainst: 0,
+      required: false
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
   render() {
-
+    console.log(this.state);
+    let name = this.state.name || '';
     return (
       <div>
         <h1>Create New Event</h1>
+        <h3>{name}</h3>
         <form>
           <div className='form-group'>
-            <label for='eventName'>Event Name</label>
-            <input type='text' className='form-control' id='eventName' aria-describedby='eventHelp' placeholder='Enter event name'/>
+            <label htmlFor='eventName'>Event Name</label>
+            <input type='text' value={name} onChange = {this.handleNameChange} className='form-control' id='eventName' aria-describedby='eventHelp' placeholder='Enter event name'/>
             <small id='eventName' className='form-text text-muted'>Enter a name for the event.</small>
           </div>
           <div className='form-group'>
-            <label for='pointsMade'>Points for Attendence</label>
+            <label htmlFor='pointsMade'>Points for Attendence</label>
             <input type='number' className='form-control' id='eventPoints' aria-describedby='eventPoints'/>
             <small id='eventPoints' className='form-text text-muted'>Enter points for attending the event.</small>
           </div>
           <div className='form-group'>
-            <label for='pointsMissed'>Points Penalty for Missed</label>
+            <label htmlFor='pointsMissed'>Points Penalty for Missed</label>
             <input type='number' className='form-control' id='eventMissed' aria-describedby='eventMissed'/>
             <small id='eventMissed' className='form-text text-muted'>Enter point penalty for missing event.</small>
           </div>
